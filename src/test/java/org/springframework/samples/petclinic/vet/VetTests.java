5e4303213b32d5985ffb64a0ff5b1936df038ecf
Thomas Gauvin <thomasgauvin.cu@gmail.com>
2020-03-16 00:18:57 -0400

    Added failing test

--- src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
@@ -35,6 +35,9 @@ class VetTests {
 		assertThat(other.getFirstName()).isEqualTo(vet.getFirstName());
 		assertThat(other.getLastName()).isEqualTo(vet.getLastName());
 		assertThat(other.getId()).isEqualTo(vet.getId());
+
+		//add failing test
+		assertEquals(true, false);
 	}
 
 }
